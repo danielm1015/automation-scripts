4a72ce1e3b1bb27d5b4edc973ce6bd14212b3bbc
danielm1015 <daniel.martinez1015@gmail.com>
2019-08-06 09:43:04 -0700

    Update seleniumautomation.py
    updated chromedriver path

--- seleniumautomation.py
+++ seleniumautomation.py
@@ -1,35 +1,35 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
 
-# launch browser & website
-driver= webdriver.Firefox()
-driver.get("http://google.com");
+dirver_path = r'C:\Users\Daniel.Martinez\Downloads\chromedriver_win32\chromedriver.exe'
+driver = webdriver.Chrome(dirver_path)
+driver.get("http://google.com")
 
 # selecting google search bar
-search = driver.find_element_by_name('q');
-search.clear();
+google_search = driver.find_element_by_name('q')
+google_search.clear()
 
 # entering & submitting text in google search bar
-search.send_keys("python wiki front page");
-search.send_keys(Keys.RETURN);
+google_search.send_keys("python wiki front page")
+google_search.send_keys(Keys.RETURN)
 time.sleep(4)
 
 # find link by text
-link = driver.find_element_by_link_textPython Wiki: FrontPage")
+link = driver.find_element_by_link_text("Python Wiki: FrontPage")
 link.click()
 
 # python wiki search bar
-searchBox= driver.find_element_by_id('searchinput')
-searchBox.clear()
+python_serchbox = driver.find_element_by_id('searchinput')
+python_serchbox.clear()
 
-searchBox.send_keys("Beginner")
-searchBox.send_keys(Keys.RETURN)
+python_serchbox.send_keys("Beginner")
+python_serchbox.send_keys(Keys.RETURN)
 time.sleep(5)
 
 # no unique id, name, or class; will use xpath:
 # html/body/div[2]/div[3]/ul/li[5]/form/div/select
-select = Select(driver.find_element_by_xpath("///*/form/div/select")
-select.select_by_visible_text("Raw Text")
+# select = Select(driver.find_element_by_xpath("///*/form/div/select")
+# select.select_by_visible_text("Raw Text")
 
-driver.close();
+driver.close()
