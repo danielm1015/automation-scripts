568ba3748a7fa57e28fe2918ce11e3c673540de6
danielm1015 <daniel.martinez1015@gmail.com>
2019-11-22 16:24:05 -0800

    Update robotframework-xml-to-csv.py
    added get start time and end time tags for each test case

--- robotframework-xml-to-csv.py
+++ robotframework-xml-to-csv.py
@@ -2,7 +2,7 @@ import csv
 import xml.etree.ElementTree as ET
 
 
-tree = ET.parse("output.xml")
+tree = ET.parse("Results\output.xml")
 root = tree.getroot()
 
 testdata = open('test.csv', 'w', newline='')
@@ -10,16 +10,14 @@ testdata = open('test.csv', 'w', newline='')
 # create the csv writer object
 csvwriter = csv.writer(testdata)
 
-row_head = ['','','','']
-
+row_head = ['A', 'B', 'C', 'D', 'C', 'E' ]
 for suite in root.findall('suite'):
     test_row = []
     csvwriter.writerow(row_head)
    
     for test in suite.findall('test'):
         for item in suite.findall('metadata/item'):
-            m = item.text
-            test_row.append(m)
+            test_row.append(item.text)
 
         test_row.append(test.get('name'))
         test_row.append(test.find('doc').text)
@@ -29,16 +27,18 @@ for suite in root.findall('suite'):
             tagdata.append(t.text)
         test_row.append(tagdata)
         test_row.append(test.find('status').get('status'))
+        test_row.append(test.find('status').get('starttime'))
+        test_row.append(test.find('status').get('endtime'))
         test_row.append(test.find('status').text)
-        steps = []
+        tests_keywords = []
         args = []
         for keyword in test.findall('kw'):
             if keyword.get('name') == 'Run Keyword And Continue On Failure':
                 for arguments in keyword.findall('arguments'):
-                    steps.append(arguments.find('arg').text)
+                    tests_keywords.append(arguments.find('arg').text)
                     for arg in arguments.findall('arg'):
                         args.append(arg.text)
-        test_row.append(steps)
+        test_row.append(tests_keywords)
         test_row.append(args)
 
         csvwriter.writerow(test_row)
